bf1b434c2b7eade0983307ad08821aca3c561221
Thirumalai-Shaktivel <thirumalaishaktivel@gmail.com>
2022-04-14 20:39:56 +0530

    Add Exception for tokenizer

---
+++ src/compy/parser/parser_exception.h
@@ -0,0 +1,31 @@
+#ifndef COMPY_PARSER_PARSER_EXCEPTION_H
+#define COMPY_PARSER_PARSER_EXCEPTION_H
+
+#include <libasr/exception.h>
+#include <libasr/diagnostics.h>
+
+namespace LFortran {
+
+namespace parser_local {
+
+    // Local exceptions that are used to terminate the tokenizer.
+    class TokenizerError
+    {
+    public:
+        diag::Diagnostic d;
+    public:
+        TokenizerError(const std::string &msg, const Location &loc)
+            : d{diag::Diagnostic(msg, diag::Level::Error, diag::Stage::Tokenizer, {
+                diag::Label("", {loc})
+            })}
+        { }
+
+        TokenizerError(const diag::Diagnostic &d) : d{d} { }
+    };
+
+}
+
+}
+
+
+#endif // COMPY_PARSER_PARSER_EXCEPTION_H
