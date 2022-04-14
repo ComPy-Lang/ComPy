3442d2fd040ca939878bc4010688b7b0b4d85b01
Thirumalai-Shaktivel <thirumalaishaktivel@gmail.com>
2022-04-14 21:05:24 +0530

    Fix the build failure

---
+++ src/compy/parser/parser_stype.h
@@ -0,0 +1,33 @@
+#ifndef COMPY1_PARSER_STYPE_H
+#define COMPY1_PARSER_STYPE_H
+
+#include <cstring>
+#include <libasr/location.h>
+#include <libasr/containers.h>
+#include <libasr/bigint.h>
+
+namespace LFortran
+{
+
+struct IntSuffix {
+    BigInt::BigInt int_n;
+};
+
+union YYSTYPE {
+    int64_t n;
+    Str string;
+    IntSuffix int_suffix;
+};
+
+static_assert(std::is_standard_layout<YYSTYPE>::value);
+static_assert(std::is_trivial<YYSTYPE>::value);
+
+} // namespace LFortran
+
+
+typedef struct LFortran::Location YYLTYPE;
+#define YYLTYPE_IS_DECLARED 1
+#define YYLTYPE_IS_TRIVIAL 0
+
+
+#endif // COMPY1_PARSER_STYPE_H
